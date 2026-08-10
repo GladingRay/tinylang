1dfebc45509bf69cdaf965a582b4475f09490eda
gr@m2 <gladingray@qq.com>
2026-08-10 20:12:25 +0800

    Add Diagnostic

---
+++ lib/Basic/Diagnostic.cpp
@@ -0,0 +1,25 @@
+#include "tinylang/Basic/Diagnostic.h"
+
+using namespace tinylang;
+
+namespace {
+const char *DiagnosticText[] = {
+#define DIAG(ID, Level, Msg) Msg,
+#include "tinylang/Basic/Diagnostic.def"
+};
+
+llvm::SourceMgr::DiagKind DiagnosticKind[] = {
+#define DIAG(ID, Level, Msg) llvm::SourceMgr::DK_##Level,
+#include "tinylang/Basic/Diagnostic.def"
+};
+
+}
+
+const char *DiagnosticsEngine::getDiagnosticText(unsigned DiagID) {
+  return DiagnosticText[DiagID];
+}
+
+llvm::SourceMgr::DiagKind
+DiagnosticsEngine::getDiagnosticKind(unsigned DiagID) {
+  return DiagnosticKind[DiagID];
+}
