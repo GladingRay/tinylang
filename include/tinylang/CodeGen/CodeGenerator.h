8af2c48f00cadb43da294977f1a912557c9b1b80
gr@m2 <gladingray@qq.com>
2026-08-23 17:49:05 +0800

    Add CodeGen

---
+++ include/tinylang/CodeGen/CodeGenerator.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "tinylang/AST/AST.h"
+#include "llvm/Target/TargetMachine.h"
+#include <string>
+
+namespace tinylang {
+class CodeGenerator {
+  llvm::LLVMContext &Ctx;
+  llvm::TargetMachine *TM;
+
+protected:
+  CodeGenerator(llvm::LLVMContext &Ctx, llvm::TargetMachine *TM)
+      : Ctx(Ctx), TM(TM) {}
+
+public:
+  static CodeGenerator *create(llvm::LLVMContext &Ctx, llvm::TargetMachine *TM);
+
+  std::unique_ptr<llvm::Module> run(ModuleDeclaration *Mod,
+                                    std::string FileName);
+};
+} // namespace tinylang
